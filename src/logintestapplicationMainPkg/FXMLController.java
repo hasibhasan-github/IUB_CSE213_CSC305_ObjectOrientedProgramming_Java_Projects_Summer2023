65ea165d04631b242dda1232703bb05ec3a68a61
@Md Hasib Hasan <>
2023-08-02 22:49:53 +0600

    All login Methods are Added

--- src/logintestapplicationMainPkg/FXMLController.java
+++ src/logintestapplicationMainPkg/FXMLController.java
@@ -5,19 +5,22 @@
 package logintestapplicationMainPkg;
 
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import static javafx.scene.control.Alert.AlertType.INFORMATION;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.input.DragEvent;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.input.TouchEvent;
 
 /**
  * FXML Controller class
@@ -32,6 +35,10 @@ public class FXMLController implements Initializable {
     private PasswordField passwordField;
     @FXML
     private Label lblEmailError;
+    @FXML
+    private Label lblpassworderror;
+    
+    private Map <String, String> emailPasswordContainer;
 
     /**
      * Initializes the controller class.
@@ -39,6 +46,11 @@ public class FXMLController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        emailPasswordContainer = new HashMap<String, String>();
+        
+        emailPasswordContainer.put("2220166@iub.edu.bd", "2220166@Iub");
+        emailPasswordContainer.put("2221793@iub.edu.bd", "2221793@Iub");
+        emailPasswordContainer.put("2111261@iub.edu.bd", "2111261@Iub");
     }    
 
     private boolean isValidEmail(String email) {
@@ -47,25 +59,18 @@ public class FXMLController implements Initializable {
         Matcher matcher = pattern.matcher(email);
         return matcher.matches();
     }
-
     
-    //private void useremailTextfieldOnValidation(ActionEvent event) {
-
-    //}
-
-    @FXML
-    private void loginButtonOnClick(ActionEvent event) {
-
+    private boolean isValidPassword(String password){
+        String passPattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=!]).{8,}$";
+        Pattern pattern = Pattern.compile(passPattern);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
     }
-
+    
     @FXML
-    private void passwordFieldOnValidation(ActionEvent event) {
-    }
-
-    @FXML
-    private void useremailTextfieldOnValidation(DragEvent event) {
+    private void ValidationProcess(MouseEvent event) {
         String email = useremailTextfield.getText().trim();
-        System.out.println(isValidEmail(email));
+        String pass = passwordField.getText();
         if (isValidEmail(email)) {
             lblEmailError.setVisible(false);
             useremailTextfield.setStyle("-fx-border-color: transparent;");
@@ -74,18 +79,59 @@ public class FXMLController implements Initializable {
             lblEmailError.setVisible(true);
             useremailTextfield.setStyle("-fx-border-color: red;");
         }
+        if ( isValidPassword(pass) ){
+            lblpassworderror.setVisible(false);
+            passwordField.setStyle("-fx-border-color: transparent");
+        }
+        else{
+            lblpassworderror.setText("*Password must content" + "\n" +
+                                    "*One Lowercase letter" + "\n" + 
+                                    "*One Uppercase letter" + "\n" +
+                                    "*One digit" + "\n" +
+                                    "*One special character");
+            lblpassworderror.setVisible(true);
+            passwordField.setStyle("-fx-border-color: red;");
+        }
     }
 
-
-
     @FXML
-    private void hasib(TouchEvent event) {
-         System.out.println("Hasib");
+    private void LoginButtonOnClick(ActionEvent event) {
+        String email = useremailTextfield.getText().trim();
+        String pass = passwordField.getText();
+        boolean done = true;
+        
+        if ( isValidEmail(email) && isValidPassword(pass) ){
+            for (Map.Entry<String, String> tmp : emailPasswordContainer.entrySet()){
+                if ( email.equals(tmp.getKey()) && pass.equals(tmp.getValue()) ){
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Successful Attempt");
+                    alert.setHeaderText("Login Successful");
+                    alert.setContentText("Welcome back to your account.");
+                    alert.showAndWait();
+                    done = false;
+                    break;
+                }
+            }
+        }
+        if (done){
+        Alert alert = new Alert(AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText("Login Attempt Failed.");
+        alert.setContentText("Invalid Email & Password Combination.");
+        alert.showAndWait();
+        }
     }
 
     @FXML
-    private void hasib2(TouchEvent event) {
-        System.out.println("Hasib");
+    private void usernameTextFieldOnAction(KeyEvent event) {
+        lblEmailError.setVisible(false);
+        useremailTextfield.setStyle("-fx-border-color: transparent;");
+    }
+
+    @FXML
+    private void passwordTextFieldOnAction(KeyEvent event) {
+            lblpassworderror.setVisible(false);
+            passwordField.setStyle("-fx-border-color: transparent;");
     }
     
 }
