40ec724d99b7221b8b0cc5f9f3511948ef4e90e9
@Md Hasib Hasan <>
2023-08-02 21:31:54 +0600

    Second

--- src/logintestapplicationMainPkg/FXMLController.java
+++ src/logintestapplicationMainPkg/FXMLController.java
@@ -6,12 +6,18 @@ package logintestapplicationMainPkg;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TouchEvent;
 
 /**
  * FXML Controller class
@@ -35,18 +41,51 @@ public class FXMLController implements Initializable {
         // TODO
     }    
 
-    @FXML
-    private void useremailTextfieldOnValidation(ActionEvent event) {
-        String email = useremailTextfield.getText();
-        
+    private boolean isValidEmail(String email) {
+        String emailPattern = "^(.+)@iub\\.edu\\.bd$";
+        Pattern pattern = Pattern.compile(emailPattern);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
+
+    
+    //private void useremailTextfieldOnValidation(ActionEvent event) {
+
+    //}
 
     @FXML
     private void loginButtonOnClick(ActionEvent event) {
+
     }
 
     @FXML
     private void passwordFieldOnValidation(ActionEvent event) {
+    }
+
+    @FXML
+    private void useremailTextfieldOnValidation(DragEvent event) {
+        String email = useremailTextfield.getText().trim();
+        System.out.println(isValidEmail(email));
+        if (isValidEmail(email)) {
+            lblEmailError.setVisible(false);
+            useremailTextfield.setStyle("-fx-border-color: transparent;");
+        } else {
+            lblEmailError.setText("Invalid Email");
+            lblEmailError.setVisible(true);
+            useremailTextfield.setStyle("-fx-border-color: red;");
+        }
+    }
+
+
+
+    @FXML
+    private void hasib(TouchEvent event) {
+         System.out.println("Hasib");
+    }
+
+    @FXML
+    private void hasib2(TouchEvent event) {
+        System.out.println("Hasib");
     }
     
 }
